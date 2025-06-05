11d5280674a22946ed016c0038459b3a2d1cd3e8
Neel Mehendale <144788333+neelSchool@users.noreply.github.com>
2025-06-05 12:01:52 +0100

    upto e save

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -20,6 +20,28 @@
             string[] subjects = {"English Language", "English Literature", "Mathematics", "Biology", "Chemistry", "Physics", "History", "Geography", "Religious Studies", "Spanish", "French", "Art & Design", "Business", "Computer Science", "Drama", "Food Preparation and Nutrition", "DT Graphics", "Music", "Physical Education", "DT Resistant Materials"};
             int mxsubjects = subjects.Length;
             int[,] grades = new int[mxstudents, mxsubjects];
+            while (true) {
+                Console.WriteLine("Options");
+                Console.WriteLine("1 for Add a Student.");
+                Console.WriteLine("2 for Enter grades for a Student.");
+                Console.WriteLine("3 for View all Student Grades.");
+                Console.WriteLine("0 for Quit.");
+                string? choice = Console.ReadLine()?.Trim();
+                switch (choice) {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting program.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                }
+            }
         }
     }
 }
